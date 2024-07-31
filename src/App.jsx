0775a4931b7b9520dc3224838df823ff490c79e5
Thenam2kx <thenam2kx@gmail.com>
2024-07-31 23:24:09 +0700

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Banner from './components/Banner'
 import Header from './components/Header'
 import MovieList from './components/MovieList'
 import MovieSearch from './components/MovieSearch'
+import img1 from '~/assets/jose-figueroa-_26mWFEuX9w-unsplash.jpg'
+import img2 from '~/assets/jose-figueroa-EX6LZpZyiss-unsplash.jpg'
+import img3 from '~/assets/jose-figueroa-HbrMwmxExTQ-unsplash.jpg'
+import img4 from '~/assets/jose-figueroa-pNTfUOsWeHc-unsplash.jpg'
 
 import { MovieProvider } from './context/MovieProvider'
 
@@ -64,6 +68,11 @@ function App() {
               )
           }
         </div>
+
+        <img src={img1} alt="" className='w-full h-[700px] object-cover mb-2' />
+        <img src={img2} alt="" className='w-full h-[700px] object-cover mb-2' />
+        <img src={img3} alt="" className='w-full h-[700px] object-cover mb-2' />
+        <img src={img4} alt="" className='w-full h-[700px] object-cover mb-2' />
       </MovieProvider>
     </>
   )
